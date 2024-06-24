d860e0029ead0d5d5f380301c67536c8ee3fda8b
German Gonzalez <germanngc@gmail.com>
2024-06-24 01:00:27 -0600

    feat(clean) Removing dist folder

--- backend/dist/routes/checkoutRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const checkoutControllers_1 = require("../controllers/checkoutControllers");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.use(authMiddleware_1.authMiddleware);
-router.post('/', checkoutControllers_1.checkout);
-exports.default = router;
